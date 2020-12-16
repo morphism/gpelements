5ffb70bdc3d852eb11b40b6133516322518f897c
Jamie Stephens <js@morphism.com>
2020-12-16 17:52:01 +0000

    Use maybe-canonicalized data in some tests

--- test.sh
+++ test.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Try to compare transformations based on Celestrak formats.  See
+# Try to compare transformations based on (canonicalized?) Celestrak
+# formats.  Base data obtained via get-test-data.sh. Also see
 # notes.md.
 
 set -e pipefail
 
+# Canonicalize base data (with the realization that we might mess up).
+for F in tle csv xml kvn jsonarray; do
+    cat data/test.$F | tletool transform -emit $F > data/canonical.$F
+done
+
+# Compare transformed data to the canonical data.
 for OUT in tle csv xml kvn jsonarray; do
 
     for IN in tle csv xml kvn jsonarray; do
 
 	      cat data/test.$IN | tletool transform -emit $OUT > data/check.$OUT
 
-	      diff -Z data/test.$OUT data/check.$OUT > data/check-$IN-$OUT.diff ||
+	      diff -Z data/canonical.$OUT data/check.$OUT > data/check-$IN-$OUT.diff ||
 		  (echo "failed: $IN $OUT" data/check-$IN-$OUT.diff)
 
     done
